80998a7fcd8c17d5a317494ecffdd6a79c137d02
RP-Guruh <github.guruh@gmail.com>
2023-10-04 17:59:27 +0700

    form after register but not completed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
     <script>
         WebFont.load({
@@ -30,6 +27,8 @@
     <link href="{{asset('css/nucleo-icons.css')}}" rel="stylesheet" />
     <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
 
+    <link href="{{asset('css/material-dashboard.css')}}" rel="stylesheet" />
+
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 
@@ -39,10 +38,9 @@
     <!-- CSS Files -->
     <link href="{{asset('css/material-dashboard.css')}}" rel="stylesheet" />
 
-    <!-- Styles -->
-
+    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
-<body>
+<body class="g-sidenav-show bg-gray-200">
 
 @yield('content')
 
@@ -51,7 +49,14 @@
 <script src="{{asset('js/core/bootstrap.min.js')}}"></script>
 <script src="{{asset('js/plugins/perfect-scrollbar.min.js')}}"></script>
 <script src="{{asset('js/plugins/smooth-scrollbar.min.js')}}"></script>
-
+<script src="{{asset('js/material-dashboard.min.js')}}"></script>
+<script src="{{asset('js/plugins/choices.min.js')}}"></script>
+<script src="{{asset('js/plugins/dropzone.min.js')}}"></script>
+<script src="{{asset('js/plugins/quill.min.js')}}"></script>
+<script src="{{asset('js/plugins/multistep-form.js')}}"></script>
+<script src="{{asset('js/plugins/dragula/dragula.min.js')}}"></script>
+<script src="{{asset('js/plugins/jkanban/jkanban.js')}}"></script>
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 
 <script>
     var win = navigator.platform.indexOf('Win') > -1;
@@ -62,10 +67,60 @@
         Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
 </script>
-
+<!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="{{asset('js/material-dashboard.min.js')}}"></script>
 
+<script>
+    if (document.getElementById('edit-deschiption')) {
+        var quill = new Quill('#edit-deschiption', {
+            theme: 'snow' // Specify theme in configuration
+        });
+    };
+
+    if (document.getElementById('choices-category')) {
+        var element = document.getElementById('choices-category');
+        const example = new Choices(element, {
+            searchEnabled: false
+        });
+    };
+
+    if (document.getElementById('choices-sizes')) {
+        var element = document.getElementById('choices-sizes');
+        const example = new Choices(element, {
+            searchEnabled: false
+        });
+    };
+
+    if (document.getElementById('choices-currency')) {
+        var element = document.getElementById('choices-currency');
+        const example = new Choices(element, {
+            searchEnabled: false
+        });
+    };
+
+    if (document.getElementById('choices-tags')) {
+        var tags = document.getElementById('choices-tags');
+        const examples = new Choices(tags, {
+            removeItemButton: true
+        });
+
+        examples.setChoices(
+            [{
+                value: 'One',
+                label: 'Expired',
+                disabled: true
+            },
+                {
+                    value: 'Two',
+                    label: 'Out of Stock',
+                    selected: true
+                }
+            ],
+            'value',
+            'label',
+            false,
+        );
+    }
+</script>
 </body>
 </html>
