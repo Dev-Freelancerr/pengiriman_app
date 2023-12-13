ff95da0650105db30f212589fcb87437e2766af0
rp-guruh <github.guruh@gmail.com>
2023-12-13 17:21:00 +0700

    fixed webhook

--- app/Http/Controllers/Api/Ninja/webhook/WebhookNinjaController.php
+++ app/Http/Controllers/Api/Ninja/webhook/WebhookNinjaController.php
@@ -26,7 +26,8 @@ class WebhookNinjaController extends Controller
     }
 
 
-    public function handledWebhookV2(Request $request) {
+    public function handledWebhookV2(Request $request)
+    {
         try {
             $data = $request->getContent();
             $hmacHeader = $request->header('x-ninjavan-hmac-sha256');
@@ -46,11 +47,15 @@ class WebhookNinjaController extends Controller
                 $shipper_order_ref_no = $payload['shipper_order_ref_no'] ?? null;
                 $timestamp = $payload['timestamp'] ?? null;
                 $status = $payload['status'] ?? null;
+                $rts_reason = $payload['rts_reason'] ?? null;
+
                 $is_parcel_on_rts_leg = $payload['is_parcel_on_rts_leg'] ?? null;
                 $state = $payload['picked_up_information']['state'] ?? null;
                 $signatureUri = $payload['picked_up_information']['proof']['signature_uri'] ?? null;
                 $imageUris = $payload['picked_up_information']['proof']['image_uris'] ?? null;
-                $signedBy = $payload['picked_up_information']['proof']['signed_by'] ?? null;
+                $signedBy_name = $payload['picked_up_information']['proof']['signed_by']['name'] ?? null;
+                $signedBy_contact = $payload['picked_up_information']['proof']['signed_by']['contact'] ?? null;
+                $signedBy_relationship = $payload['picked_up_information']['proof']['signed_by']['relationship'] ?? null;
                 $arrived_at_pudo_information = $payload['arrived_at_pudo_information']['state'] ?? null;
 
                 $pickup_exception_state = $payload['pickup_exception']['state'] ?? null;
@@ -79,9 +84,11 @@ class WebhookNinjaController extends Controller
 
                 $arrived_at_pudo_information_state = $payload['arrived_at_pudo_information']['state'] ?? null;
 
-                $delivery_information_state = $payload['delivery_information']['state'];
-                $delivery_information_left_in_safe_place = $payload['delivery_information']['left_in_safe_place'];
-                $delivery_information_signedBy = $payload['delivery_information']['proof']['signed_by'] ?? null;
+                $delivery_information_state = $payload['delivery_information']['state'] ?? null;
+                $delivery_information_left_in_safe_place = $payload['delivery_information']['left_in_safe_place'] ?? null;
+                $delivery_information_signedBy_contact = $payload['delivery_information']['proof']['signed_by']['contact'] ?? null;
+                $delivery_information_signedBy_name = $payload['delivery_information']['proof']['signed_by']['name'] ?? null;
+                $delivery_information_signedBy_relationship = $payload['delivery_information']['proof']['signed_by']['relationship'] ?? null;
 
                 $delivery_information_image_uris = $payload['delivery_information']['proof']['image_uris'] ?? null;
 
@@ -94,8 +101,14 @@ class WebhookNinjaController extends Controller
                 $delivery_exception_image_uris = $payload['delivery_exception']['proof']['image_uris'] ?? null;
                 $cancellation_information = $payload['cancellation_information']['reason'] ?? null;
 
-                $imageUrisString = implode(', ', $imageUris ?? $imageUris_exception ?? $delivery_information_image_uris ?? $delivery_exception_image_uris);
-
+                // $imageUrisString = implode(', ', $imageUris ?? $imageUris_exception ?? $delivery_information_image_uris ?? $delivery_exception_image_uris);
+                    // Memeriksa apakah semua variabel null
+                    if ($imageUris === null && $imageUris_exception === null && $delivery_information_image_uris === null && $delivery_exception_image_uris === null) {
+                        $imageUrisString = null;
+                    } else {
+                        // Menggunakan implode dengan nilai default array kosong jika variabel adalah null
+                        $imageUrisString = implode(', ', $imageUris ?? $imageUris_exception ?? $delivery_information_image_uris ?? $delivery_exception_image_uris);
+                    }
 
 
                 $data_order = [
@@ -104,23 +117,25 @@ class WebhookNinjaController extends Controller
                     'timestamp' => Carbon::parse($timestamp),
                     'tracking_id' => $tracking_id,
                     'is_parcel_on_rts_leg' => $is_parcel_on_rts_leg,
-                    'state' => $state ?? $pickup_exception_state ?? $arrived_at_pudo_information_state ?? $delivery_information_state ?? $delivery_exception_state,
-                    'signature_uri' => $signatureUri ?? $delivery_information_signature_uri,
-                    'image_uris' => $imageUrisString,
-                    'signed_by_contact' => $signedBy['contact'] ?? $delivery_information_signedBy['contact'],
-                    'signed_by_name' => $signedBy['name'] ?? $delivery_information_signedBy['name'],
-                    'signed_by_relationship' => $signedBy['relationship'] ?? $delivery_information_signedBy['relationship'],
-                    'arrived_at_pudo_information_state' => $$arrived_at_pudo_information,
-                    'failure_reason' =>  $pickup_exception_failure_reason ?? $delivery_exception_failure_reason,
-                    'is_liable' => $pickup_exception_is_liable ?? $delivery_exception_is_liable,
-                    'country' => $country ?? $country_transit ?? $country_destination ?? $country_sorting,
-                    'city' => $city ?? $city_transit ?? $city_destination ?? $city_sorting,
-                    'hub' => $hub ?? $hub_transit ?? $hub_destination ?? $hub_sorting,
                     'driver_contact' => $driver_contact,
                     'allow_doorstep_dropoff' => $allow_doorstep_dropoff,
                     'driver_name' => $driver_name,
                     'left_in_safe_place' => $delivery_information_left_in_safe_place,
-                    'reason_cancel' => $cancellation_information
+                    'reason_cancel' => $cancellation_information,
+                    'reason' => $rts_reason,
+                    'state' => $state ?? $pickup_exception_state ?? $arrived_at_pudo_information_state ?? $delivery_information_state ?? $delivery_exception_state,
+                    'signature_uri' => $signatureUri ?? $delivery_information_signature_uri,
+                    'image_uris' => $imageUrisString,
+                    'signed_by_contact' => $signedBy_contact ?? $delivery_information_signedBy_contact,
+                    'signed_by_name' => $signedBy_name ?? $delivery_information_signedBy_name,
+                    'signed_by_relationship' => $signedBy_relationship ?? $delivery_information_signedBy_relationship,
+                    'arrived_at_pudo_information_state' => $arrived_at_pudo_information,
+                    'failure_reason' => $pickup_exception_failure_reason ?? $delivery_exception_failure_reason,
+                    'is_liable' => $pickup_exception_is_liable ?? $delivery_exception_is_liable,
+                    'country' => $country ?? $country_transit ?? $country_destination ?? $country_sorting,
+                    'city' => $city ?? $city_transit ?? $city_destination ?? $city_sorting,
+                    'hub' => $hub ?? $hub_transit ?? $hub_destination ?? $hub_sorting,
+
                 ];
 
                 TrackNinja::create($data_order);
