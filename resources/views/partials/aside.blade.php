cac34c46f4ddb3b0582b6b6d82b7fa71bd947b36
rp-guruh <github.guruh@gmail.com>
2024-02-03 21:05:43 +0700

    a

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -5,7 +5,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
            aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0"
-           href="https://www.app.alifa.web.id"
+           href="app.alifa.web.id"
            target="_blank">
             <img src="{{asset('img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold text-white">Pengiriman Dashboard</span>
