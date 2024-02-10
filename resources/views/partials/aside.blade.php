c7652a2fdf5f80dd4247bb06dec725cfe15dc5f4
rp-guruh <github.guruh@gmail.com>
2024-02-11 00:10:43 +0700

    11-feb-2024 00.10 tambah link logout

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -4,7 +4,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
            aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="app.alifa.web.id" target="_blank">
+        <a class="navbar-brand m-0" href="/">
             <img src="{{asset('img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold text-white">Pengiriman Dashboard</span>
         </a>
@@ -65,6 +65,19 @@
                     <span class="nav-link-text ms-2 ps-1">Estimasi Tarif</span>
                 </a>
             </li>
+
+
+     <li class="nav-item">
+    <a href="{{ route('logout') }}" class="nav-link text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <i class="material-icons-round">exit_to_app</i>
+        <span class="nav-link-text ms-2 ps-1">Logout</span>
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+</li>
+
+
         </ul>
     </div>
 </aside>
